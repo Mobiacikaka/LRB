d3d236a90067eac6f888613fc63f25b36ff58f13
Mobiacikaka <stable.justin@gmail.com>
2021-12-10 13:08:38 +0800

    new datasets generate approach

--- generate_datasets.py
+++ generate_datasets.py
@@ -1,23 +1,30 @@
+import scipy.stats as stats
 import numpy as npy
 import os
 
 def generate_dataset(n_of_dataset: int, srv_no: int, server_number: int):
-    mask = 0xFFF
-    # mu = mask >> 1
-    mu = mask * srv_no / server_number
-    sigma = 256
-    s = npy.random.normal(mu, sigma, n_of_dataset)
-    s = s.astype(int)
-    s = npy.asarray([e % mask for e in s])
     filename = r'./datasets/dataset_' + str(srv_no) + '.txt'
-    s.tofile(filename, '\t', "%s")
+
+    set1_mask = 0xFFFF
+    set1_size = n_of_dataset >> 1;
+    unif = npy.random.uniform(0, set1_mask, set1_size)
+    unif = [int(e) for e in unif]
+
+    set2_size = n_of_dataset - set1_size
+    for i in range(1, set2_size):
+        addition = (i + srv_no) % set2_size
+        unif.append(set1_mask + addition)
+
+    unif = npy.asarray(unif)
+    unif.tofile(filename, "\t", "%s")
+
 
 if __name__ == '__main__':
     os.system(r'rm datasets/* -rf')
     npy.random.seed(0)
     server_number = 1000
-    base_n = 1000
-    rng = base_n / 10
+    base_n = 1024
+    rng = 0
     for srv_no in range(server_number):
-        n_of_dataset = npy.random.randint(base_n-rng, base_n+rng)
+        n_of_dataset = base_n if rng == 0 else npy.random.randint(base_n-rng, base_n+rng)
         generate_dataset(n_of_dataset, srv_no, server_number)
