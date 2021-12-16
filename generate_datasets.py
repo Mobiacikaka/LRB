813d4d30307685ad5fccf7f616ede2c6ecd32e05
Mobiacikaka <stable.justin@gmail.com>
2021-12-16 12:57:00 +0800

    update Generate Approach A

--- generate_datasets.py
+++ generate_datasets.py
@@ -1,31 +1,20 @@
-import scipy.stats as stats
 import numpy as npy
 import os
 
-def generate_dataset(n_of_dataset: int, srv_no: int, hf_ratio: float):
+def generate_dataset(n_of_dataset: int, srv_no: int, server_number: int):
+    mask = 0xFFF
+    mu = mask * srv_no / server_number
+    sigma = 256
+    s = npy.random.normal(mu, sigma, n_of_dataset)
+    s = s.astype(int)
+    s = npy.asarray([e % mask for e in s])
     filename = r'./datasets/dataset_' + str(srv_no) + '.txt'
-
-    set1_mask = 0xFFFF
-    set1_size = int(n_of_dataset * hf_ratio)
-    unif = npy.random.uniform(0, set1_mask, set1_size)
-    unif = [int(e) for e in unif]
-
-    set2_size = n_of_dataset - set1_size
-    for i in range(1, set2_size):
-        addition = (i + srv_no) % set2_size
-        unif.append(set1_mask + addition)
-
-    unif = npy.asarray(unif)
-    unif.tofile(filename, "\t", "%s")
-
+    s.tofile(filename, '\t', "%s")
 
 if __name__ == '__main__':
     os.system(r'rm datasets/* -rf')
     npy.random.seed(0)
-    server_number = 1000
-    base_n = 1024
-    rng = 0
-    hf_ratio = float(input())
+    server_number = 10000
+    n_of_dataset = 1024
     for srv_no in range(server_number):
-        n_of_dataset = base_n if rng == 0 else npy.random.randint(base_n-rng, base_n+rng)
-        generate_dataset(n_of_dataset, srv_no, hf_ratio)
+        generate_dataset(n_of_dataset, srv_no, server_number)
